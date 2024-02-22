3b0270840b12c759bf6d807646ee3b2432b4764c
Fabio <fabiot92@gmail.com>
2024-02-22 17:50:47 -0300

    data and database conection

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Ana",
+                        "Ana@gmail.com",
+                        LocalDate.of(1999, Month.APRIL, 5),
+                        21
+                )
+        );
+    }
+
+
+}
