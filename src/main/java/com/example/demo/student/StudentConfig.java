bb2b4e5c258180b34041a2bb3a2df6ed483139cf
Fabio <fabiot92@gmail.com>
2024-02-23 15:38:16 -0300

    att
    att

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args->{
+            Student ana = new Student(
+
+                    "Ana",
+                    "Ana@gmail.com",
+                    LocalDate.of(1999, Month.APRIL, 5),
+                    25
+            );
+            Student bia = new Student(
+
+                    "Bia",
+                    "Bia@gmail.com",
+                    LocalDate.of(1992, Month.APRIL, 5),
+                    31
+            );
+            repository.saveAll(
+                    List.of(ana,bia)
+            );
+
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +11,16 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    @GetMapping
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Ana",
-                        "Ana@gmail.com",
-                        LocalDate.of(1999, Month.APRIL, 5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 
 
